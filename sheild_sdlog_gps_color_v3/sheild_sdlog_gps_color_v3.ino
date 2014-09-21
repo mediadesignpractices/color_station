3820c61f48b168ab97beff122a15e5565357bf04
unsalted <nick@unsalted.nu>
2014-09-20 23:21:49 -0700

    logging responses

--- sheild_sdlog_gps_color_v3/sheild_sdlog_gps_color_v3.ino
+++ sheild_sdlog_gps_color_v3/sheild_sdlog_gps_color_v3.ino
@@ -2,28 +2,31 @@
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 #include <SD.h>
-#include "Adafruit_TCS34725.h"
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
 #include <avr/sleep.h>
 
-// Ladyada's logger modified by Bill Greiman to use the SdFat library
+// Code hacked together by Nick Meehan nick@unsalted.nu -
+// In association with: Art Center College of Design - Media Design Practices - MFA
 //
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
+// This code is a combination of two code sets:
+// https://github.com/adafruit/Adafruit-GPS-Library/tree/master/examples/shield_sdlog
+// https://github.com/adafruit/Adafruit_TCS34725/tree/master/examples/tcs34725
 //
-// Tested and works great with the Adafruit Ultimate GPS Shield
-// using MTK33x9 chipset
-//    ------> http://www.adafruit.com/products/
-// Pick one up today at the Adafruit electronics shop 
-// and help support open source hardware & software! -ada
-// Fllybob added 10 sec logging option
-SoftwareSerial mySerial(8, 7);
+// With additional help from: http://www.instructables.com/id/Personal-Black-Box-Arduino-Mega-Ultimate-GPS-Shiel/
+// The goal of this code is to map coordinates with color data taken by TCS34725
+//
+//
+// Developed on the Adafruit Ultimate GPS + Logging Shield & TCS34725 RGB Color Sensor
+// on an Arduino Mega 2560
+// 
+
+HardwareSerial mySerial = Serial1;    //This line is only for mega
 Adafruit_GPS GPS(&mySerial);
 
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
-#define GPSECHO  true
+#define GPSECHO  false
 /* set to true to only log to SD when GPS has a fix, for debugging, keep it false */
 #define LOG_FIXONLY false  
 
@@ -41,16 +44,17 @@ File logfile;
 // read a Hex value and return the decimal equivalent
 uint8_t parseHex(char c) {
   if (c < '0')
-    return 0;
+  return 0;
   if (c <= '9')
-    return c - '0';
+  return c - '0';
   if (c < 'A')
-    return 0;
+  return 0;
   if (c <= 'F')
-    return (c - 'A')+10;
+  return (c - 'A')+10;
 }
 
-// blink out an error code
+// Errors not vurrently used on board but could come in handy.
+// Blink out an error code
 void error(uint8_t errno) {
   /*
   if (SD.errorCode()) {
@@ -60,7 +64,7 @@ void error(uint8_t errno) {
    Serial.println(card.errorData(), HEX);
    }
    */
-  while(1) {
+   while(1) {
     uint8_t i;
     for (i=0; i<errno; i++) {
       digitalWrite(ledPin, HIGH);
@@ -77,18 +81,15 @@ void error(uint8_t errno) {
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 void setup() {
-  // for Leonardos, if you want to debug SD issues, uncomment this line
-  // to see serial output
-  //while (!Serial);
   
   if (tcs.begin()) {
     Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1);
-  }
-  
-  tcs.setInterrupt(true);
+    } else {
+      Serial.println("No TCS34725 found ... check your connections");
+      while (1);
+    }
+
+    tcs.setInterrupt(true);
 
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   // also spit it out
@@ -101,16 +102,18 @@ void setup() {
   pinMode(10, OUTPUT);
 
   // see if the card is present and can be initialized:
-  //if (!SD.begin(chipSelect, 11, 12, 13)) {
-  if (!SD.begin(chipSelect)) {      // if you're using an UNO, you can use this line instead
-    Serial.println("Card init. failed!");
-    error(2);
-  }
-  char filename[15];
-  strcpy(filename, "GPSLOG00.CSV");
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[6] = '0' + i/10;
-    filename[7] = '0' + i%10;
+  if (!SD.begin(chipSelect, 11, 12, 13)) {
+  Serial.println("Card init. failed!");
+  error(2);
+}
+
+
+char filename[15];
+strcpy(filename, "GPSLOG00.CSV");
+
+for (uint8_t i = 0; i < 100; i++) {
+  filename[6] = '0' + i/10;
+  filename[7] = '0' + i%10;
     // create if does not exist, do not open existing, write, sync after write
     if (! SD.exists(filename)) {
       break;
@@ -125,8 +128,9 @@ void setup() {
   }
   Serial.print("Writing to "); 
   Serial.println(filename);
-  //logfile.println("Time, Date, Latitude, Longitude, Elevation, Speed (Knots), Angle, Satellites");
-  //logfile.flush();
+  // Print csv header
+  logfile.println("Time, Date, Latitude, Longitude, Elevation, Speed (Knots), Angle, Satellites, Color Temp, Lux, R, G, B, C");
+  logfile.flush();
 
   // connect to the GPS at the desired rate
   GPS.begin(9600);
@@ -149,6 +153,8 @@ void setup() {
   useInterrupt(true);
 
   Serial.println("Ready!");
+  // Print CSV headers to serial
+  Serial.println("Time, Date, Latitude, Longitude, Elevation, Speed (Knots), Angle, Satellites, Color Temp, Lux, R, G, B, C");
 }
 
 
@@ -157,15 +163,15 @@ SIGNAL(TIMER0_COMPA_vect) {
   char c = GPS.read();
   // if you want to debug, this is a good time to do it!
   #ifdef UDR0
-      if (GPSECHO)
-        if (c) UDR0 = c;  
+  if (GPSECHO)
+  if (c) UDR0 = c;  
       // writing direct to UDR0 is much much faster than Serial.print 
       // but only one character can be written at a time. 
-  #endif
-}
+      #endif
+    }
 
-void useInterrupt(boolean v) {
-  if (v) {
+    void useInterrupt(boolean v) {
+      if (v) {
     // Timer0 is already used for millis() - we'll just interrupt somewhere
     // in the middle and call the "Compare A" function above
     OCR0A = 0xAF;
@@ -179,13 +185,25 @@ void useInterrupt(boolean v) {
   }
 }
 
+
 void loop() {
+
+  tcs.setInterrupt(true);      // turn on LED
+
+  // Declare color sensor variables
+  uint16_t r, g, b, c, colorTemp, lux;
+  
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+  
+  
   if (! usingInterrupt) {
     // read data from the GPS in the 'main loop'
     char c = GPS.read();
     // if you want to debug, this is a good time to do it!
     if (GPSECHO)
-      if (c) Serial.print(c);
+    if (c) Serial.print(c);
   }
   
   // if a sentence is received, we can check the checksum, parse it...
@@ -203,15 +221,15 @@ void loop() {
       return;  // we can fail to parse a sentence in which case we should just wait for another
 
     // Sentence parsed! 
-    Serial.println("OK");
+    Serial.println("GPS parsed OK");
     if (LOG_FIXONLY && !GPS.fix) {
       Serial.print("No Fix");
       return;
     }
 
-    // Rad. lets log it!
-    Serial.println("Log");
-      
+    // Super. log it!
+    Serial.println("Log: start");
+    //Time  
     logfile.print(GPS.hour, DEC);
     logfile.print(':');
     logfile.print(GPS.minute, DEC);
@@ -220,14 +238,14 @@ void loop() {
     logfile.print('.');
     logfile.print(GPS.milliseconds);
     logfile.print(",");
-
+    //Date
     logfile.print(GPS.month, DEC); 
     logfile.print('/');
     logfile.print(GPS.day, DEC);
     logfile.print("/20");
     logfile.print(GPS.year, DEC);
     logfile.print(",");
-
+    //Coordinates
     logfile.print(GPS.latitude, 4);
     logfile.print(GPS.lat);
     logfile.print(",");
@@ -240,9 +258,71 @@ void loop() {
     logfile.print(",");
     logfile.print(GPS.angle);
     logfile.print(",");
-    logfile.println((int)GPS.satellites);
+    logfile.print((int)GPS.satellites);
+    logfile.print(",");
+    //Color
+    logfile.print(colorTemp, DEC); 
+    logfile.print(",");
+    logfile.print(lux, DEC); 
+    logfile.print(",");
+    logfile.print(r, DEC); 
+    logfile.print(",");
+    logfile.print(g, DEC); 
+    logfile.print(",");
+    logfile.print(b, DEC); 
+    logfile.print(",");
+    logfile.println(c, DEC);
+    // Wait for logging to finish
     logfile.flush();
-    Serial.println("success."); 
+    // Print to serial
+    Serial.println("Logging: complete");
+    Serial.println();
+    Serial.println("Logged Data:");
+    // Time
+    Serial.print(GPS.hour, DEC);
+    Serial.print(':');
+    Serial.print(GPS.minute, DEC);
+    Serial.print(':');
+    Serial.print(GPS.seconds, DEC);
+    Serial.print('.');
+    Serial.print(GPS.milliseconds);
+    Serial.print(",");
+    // Date
+    Serial.print(GPS.month, DEC); 
+    Serial.print('/');
+    Serial.print(GPS.day, DEC);
+    Serial.print("/20");
+    Serial.print(GPS.year, DEC);
+    Serial.print(",");
+    // Coordinates
+    Serial.print(GPS.latitude, 4);
+    Serial.print(GPS.lat);
+    Serial.print(",");
+    Serial.print(GPS.longitude, 4);
+    Serial.print(GPS.lon);
+    Serial.print(",");
+    Serial.print(GPS.altitude);
+    Serial.print(",");
+    Serial.print(GPS.speed);
+    Serial.print(",");
+    Serial.print(GPS.angle);
+    Serial.print(",");
+    Serial.print((int)GPS.satellites);
+    Serial.print(",");
+    // Color temp
+    Serial.print(colorTemp, DEC); 
+    Serial.print(",");
+    Serial.print(lux, DEC); 
+    Serial.print(",");
+    Serial.print(r, DEC); 
+    Serial.print(",");
+    Serial.print(g, DEC); 
+    Serial.print(",");
+    Serial.print(b, DEC); 
+    Serial.print(",");
+    Serial.println(c, DEC);
+
+    Serial.print("\n");
     Serial.println();
   }
 }
